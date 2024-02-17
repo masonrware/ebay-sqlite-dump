fc869917c6c9d70e75f8f6ec8fe8460904d48a81
Ben Brandl <benbrandl82@gmail.com>
2024-02-17 13:34:29 -0600

    query 6 fix

--- query6.sql
+++ query6.sql
@@ -1,10 +1,9 @@
 DROP VIEW IF EXISTS sellers;
 CREATE VIEW sellers AS
-SELECT DISTINCT I.sellerid AS ID, U.rating AS Rating
-FROM users U, items I
-WHERE U.userid = I.sellerid;
+SELECT DISTINCT I.sellerid AS ID
+FROM items I;
 
 SELECT COUNT(*)
-FROM    (SELECT DISTINCT *
+FROM    (SELECT DISTINCT S.id
     FROM sellers S, bids B
     WHERE S.id = B.userid);
